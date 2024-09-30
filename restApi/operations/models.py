7d3d05a69ce8f104813f7000c22691e39ff87006
pavansta <pavan.learn69@gmail>
2024-09-30 10:51:53 +0530

    created moded,push to

--- restApi/operations/models.py
+++ restApi/operations/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+class Programer(models.Model):
+    username = models.CharField(max_length=120)
+    bio = models.TextField(max_length=250,null=True,blank=True)
+    
+    
