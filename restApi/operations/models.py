9e096d71d1b8f0aacdf5e9f5bda38e4304d11cc2
pavansta <pavan.learn69@gmail>
2024-09-30 11:03:34 +0530

    small update in models

--- restApi/operations/models.py
+++ restApi/operations/models.py
@@ -1,8 +1,10 @@
 from django.db import models
 
+
 # Create your models here.
 class Programer(models.Model):
     username = models.CharField(max_length=120)
-    bio = models.TextField(max_length=250,null=True,blank=True)
-    
-    
+    bio = models.TextField(max_length=250, null=True, blank=True)
+
+    def __str__(self):
+        return self.username
